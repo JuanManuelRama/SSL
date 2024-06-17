0c61b753f31b68df33ffa2e9d3a7a24e904df914
Juanma <jrama@frba.utn.edu.ar>
2024-06-17 13:04:19 -0300

    Completo tp

--- 03-Historigrama/histograma-1-enum-switch.c
+++ 03-Historigrama/histograma-1-enum-switch.c
@@ -10,7 +10,7 @@ int* contar(){
     STATE state;
     state = OUT;
     length = 0;
-    while ((c = getchar()) != 51) {
+    while ((c = getchar()) != EOF) {
         switch(state){
             case OUT:
                 if(!(c == ' ' || c == '\n' || c == '\t')){
